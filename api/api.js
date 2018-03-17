ea4576546fc1a6fad3403da7ef42f025a7a1c217
ClutteredMind <yutaka@clutteredmind.com>
2018-03-17 10:59:05 -0700

    Updated package.json. Stubbed out mock image post route.

--- api/api.js
+++ api/api.js
@@ -14,6 +14,11 @@ const router = require('express').Router();
 const util = require('./api-util');
 const logger = require('../logger');
 
+const {
+    Image,
+    loadImage
+} = require('canvas');
+
 router.get('/', (req, res) => {
     res.json({
         this: 'is the api'
@@ -22,13 +27,30 @@ router.get('/', (req, res) => {
 
 router.post('/mock', (req, res) => {
     if (req.body.stringToMock != undefined) {
-        res.json({
-            mockedString: util.mock(req.body.stringToMock)
+        var mockedString = util.mock(req.body.stringToMock);
+
+        var imageFile = fs.readFileSync(path.join(__dirname, '../', 'images', 'sponge.png'));
+        var image = new Image();
+        image.src = imageFile;
+        var newCanvas = new canvas(650, 381);
+        var context = newCanvas.getContext('2d');
+
+        // draw the image to the canvas
+        context.drawImage(image, 0, 0, image.width, image.height);
+
+        context.strokeStyle = 'rgba(0,0,0,0.5)';
+        context.font = '30px Impact';
+
+        var textMetrics = context.measureText(mockedString);
+        context.fillText(mockedString, 0, 30);
+
+        res.send({
+            url: newCanvas.toDataURL()
         });
     } else {
         logger.log('error', 'Incorrect POST body: ' + JSON.stringify(req.body));
         res.json({
-            error: 'Incorrect POST body.'
+            error: 'Incorrect POST body. Expected "stringToMock"'
         });
     }
 });
